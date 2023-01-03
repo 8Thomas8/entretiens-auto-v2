8d0f52a0dc7865610db3baa9876633840b51e3e3
Thomas Rousseau <thomas.rousseau@proton.me>
2023-01-03 23:12:00 +0100

    Ajout type et enum pour les véhicules

---
+++ enum/Energy.ts
@@ -0,0 +1,6 @@
+export enum Energy {
+    electric = "Electrique",
+    diesel  = "Diesel",
+    gasoline = "Essence",
+    hybrid = "Hybride"
+}
---
+++ types/vehicle.d.ts
@@ -0,0 +1,11 @@
+declare interface IVehicle {
+    id?: number;
+    created_at: string;
+    updated_at: string;
+    created_by: string;
+    updated_by: string;
+    builder: string;
+    energy: string;
+    km: number;
+    name: string;
+}
